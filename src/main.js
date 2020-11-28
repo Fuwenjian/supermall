76c02bd0977e9bae5832533e26be3f4c074f5a10
Fuwenjian <27433864@qq.com>
2020-11-28 22:10:51 +0800

    减少移动端300ms延迟 以及图片懒加载

--- src/main.js
+++ src/main.js
@@ -2,12 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+//解决移动端300ms延迟
+import FastClick from "fastclick"
+
+//图片懒加载插件
+import VueLazyLoad from "vue-lazyload"
+//第二个可以根据loading设置图片在加载的时候默认的图片
+Vue.use(VueLazyLoad,{
+  loading:require('./assets/image/smart/showphoto.jpg')
+})
+
 //引入toast插件
 import toast from "components/common/toast"
 import "./assets/css/iconfont/iconfont.css"
-Vue.config.productionTip = false
 //插入toast插件
 Vue.use(toast)
+
+//解决移动端300ms延迟
+FastClick.attach(document.body)
+
+Vue.config.productionTip = false
 
 //事件总线
 Vue.prototype.$bus = new Vue()
